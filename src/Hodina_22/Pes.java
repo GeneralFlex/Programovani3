b0524d4a4666323b3910dd874f2131da36275c08
GeneralFlex <kloudavincent@gmail.com>
2026-04-30 09:14:38 +0200

    zvire

---
+++ src/Hodina_22/Pes.java
@@ -0,0 +1,14 @@
+package Hodina_22;
+
+public class Pes extends Zvire {
+    @Override
+    public int kolikMamNohou() {
+        return 4;
+    }
+    public void nastavJmeno(String noveJmeno){
+
+    }
+    public String toString() {
+        return "nohou: "+kolikMamNohou();
+    }
+}
---
+++ src/Hodina_22/Zvire.java
@@ -0,0 +1,5 @@
+package Hodina_22;
+
+public abstract class Zvire {
+    abstract public int kolikMamNohou();
+}
